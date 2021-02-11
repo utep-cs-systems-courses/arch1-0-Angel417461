15bf46e67eb0298030f3889bebb0d5f6b8070bcc
Angel Rodriguez II <rodriguez4174@gmail.com>
2021-02-11 10:40:16 -0700

    adding all files that were modified for project 0

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, chars_other_font = d, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -16,6 +16,10 @@ int main()
 
     // Given the user answer, select which method to call
     switch (c) {
+    case 'a':
+      puts("you selected arrow:");
+      print_arrow(3, 9);
+      break;
     case 't':
       puts("You selected triangle:");
       print_triangle(5, 7);
@@ -28,6 +32,11 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+      case 'd':
+      puts("You selected chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
